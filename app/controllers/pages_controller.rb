18a35d9e25f5137308b8add4360618db3e432e74
DotHide <dothide@gmail.com>
2015-01-31 22:28:52 +0800

    矫正时区

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -5,7 +5,7 @@ class PagesController < ApplicationController
   private 
   
     def offline
-      start_day = Time.local('2015', '1', '31', '22', '50', '00').to_i
+      start_day = Time.local('2015', '1', '31', '12', '50', '00').to_i
       end_day = Time.local('2015', '2', '1', '12', '30', '00').to_i
 
       if (Time.new.to_i - start_day) < 0 || (end_day - Time.new.to_i) < 0
