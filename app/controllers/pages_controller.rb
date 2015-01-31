4f08517650545277f9005dd490073a53b82fe1fe
DotHide <dothide@gmail.com>
2015-01-31 22:21:53 +0800

    搞定 High_voltage

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,4 +1,16 @@
 class PagesController < ApplicationController
   include HighVoltage::StaticPage
+  before_action :offline
+
+  private 
+  
+    def offline
+      start_day = Time.local('2015', '1', '31', '22', '50', '00').to_i
+      end_day = Time.local('2015', '2', '1', '12', '30', '00').to_i
+
+      if (Time.new.to_i - start_day) < 0 || (end_day - Time.new.to_i) < 0
+        render page_path('thx')
+      end
+    end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -10,6 +10,10 @@ Rails.application.routes.draw do
 
   get 'agenda/index'
 
+  get "/pages/*id" => 'pages#show', as: :page, format: false
+  root to: 'pages#show', id: 'home'
+
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
